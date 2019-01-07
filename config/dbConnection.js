e0552ddd44286f73db1d0244847d9dd5ac8e0b56
Vinicius Silva <viniciusfs.ecp@gmail.com>
2019-01-07 19:08:15 -0200

    Inserido gif loader nas requisições ajax

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,7 @@
 var mongo = require("mongodb").MongoClient;
 var assert = require("assert");
 
-const cloud = true;
+const cloud = false;
 
 var mongodbHost = 'localhost';
 var mongodbPort = '27017';
